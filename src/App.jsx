05977c24de01bc63ea1d74449e6d3ea65dd36506
MrudulSuryawanshi <suryawanshimrudul2006@gmail.com>
2026-06-13 11:49:14 +0530

    Form using 1 Object or State

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,34 @@
-import React from 'react';
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 import "./App.css";
 
 const App = () => {
-	const [firstName,setFirstName] = useState("");
-	const [lastName,setLastName] = useState("");
-	const [email,setEmail] = useState("");
-	const [password,setPassword] = useState("");
-	const [contactNo,setContactNo] = useState("");
-	const [address,setAddress] = useState("");
-	const [description,setDescription] = useState("");
-  const [users,setUsers] = useState([]);
+  const [users, setUsers] = useState([]);
   const [editUserId, setEditUserId] = useState(null);
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    contactNo: "",
+    address: "",
+    description: "",
+  });
 
-  const adduser = () => {
+const handleChange = (e) => {
+  setFormData({
+    ...formData,
+    [e.target.name]: e.target.value,
+    });
+};
 
+const adduser = () => {
   if (editUserId !== null) {
-
     const updatedUsers = users.map((user) => {
-
       if (user.id === editUserId) {
-
-        console.log("User with spread operator:", { ...user, firstName, lastName, email, contactNo, address, description });
-        console.log("User without spread operator:", { user,firstName, lastName, email, contactNo, address, description });
-
         return {
           ...user,
-          firstName,
-          lastName,
-          email,
-          contactNo,
-          address,
-          description
+          ...formData,
         };
       }
 
@@ -40,124 +37,174 @@ const App = () => {
 
     setUsers(updatedUsers);
     setEditUserId(null);
-
-  } else {
-
+  }
+  else {
     const user = {
       id: Date.now(),
-      firstName,
-      lastName,
-      email,
-      contactNo,
-      address,
-      description
+      ...formData,
     };
-
     setUsers([...users, user]);
-    // setUsers((prevUsers) => [...prevUsers, user]);
   }
 
-  setFirstName("");
-  setLastName("");
-  setEmail("");
-  setPassword("");
-  setContactNo("");
-  setAddress("");
-  setDescription("");
+  setFormData({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    contactNo: "",
+    address: "",
+    description: "",
+  });
 };
+
 const deleteUser = (id) => {
-  const updatedUsers = users.filter(
-    (user) => user.id !== id
-  );
+  const updatedUsers = users.filter((user) => user.id !== id);
 
-  setUsers(updatedUsers);
+    setUsers(updatedUsers);
 };
-    const editUser = (user) => {
-    setFirstName(user.firstName);
-    setLastName(user.lastName);
-    setEmail(user.email);
-    setContactNo(user.contactNo);
-    setAddress(user.address);
-    setDescription(user.description);
 
+const editUser = (user) => {
+  setFormData({
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+    password: user.password || "",
+    contactNo: user.contactNo,
+    address: user.address,
+    description: user.description,
+  });
   setEditUserId(user.id);
-};
-return(
-	<div className='form-container'>
-	  <form>
-	  {
-		  <div>
-        <h1>Form using React</h1>
-		  	<label htmlFor="firstname"> First Name </label>
-		  	<input type = "text"  placeholder='First Name' value={firstName} onChange={(e) => setFirstName(e.target.value)} required/>
-        <br /><br />
+  };
+  return (
+    <div className="form-container">
+      <form>
+        {
+          <div>
+            <h1>Form using React</h1>
+            <label htmlFor="firstname"> First Name </label>
+            <input
+              type="text"
+              name="firstName"
+              placeholder="First Name"
+              value={formData.firstName}
+              onChange={handleChange}
+            />
+            <br />
+            {/* <br /> */}
 
-        <label htmlFor="lastname"> Last Name </label>
-		  	<input type = "text"  placeholder='Last Name' value={lastName} onChange={(e) => setLastName(e.target.value)} required/>
-        <br /><br />
+            <label htmlFor="lastname"> Last Name </label>
+            <input
+              type="text"
+              name="lastName"
+              placeholder="Last Name"
+              value={formData.lastName}
+              onChange={handleChange}
+              required
+            />
+            <br />
+            {/* <br /> */}
 
-        <label htmlFor="email"> Email </label>
-			  <input type="text"  placeholder='Enter Email' value={email} onChange={(e) => setEmail(e.target.value)} required/>
-        <br /><br/>
+            <label htmlFor="email"> Email </label>
+            <input
+              type="text"
+              name="email"
+              placeholder="Enter Email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+            <br />
+            {/* <br /> */}
 
-        <label htmlFor="password"> Password </label>
-	  		<input type="password"  placeholder='Enter Password' value={password} onChange={(e) => setPassword(e.target.value)} required/>
-        <br /><br/>
-        
-        <label htmlFor="contactno"> Contact Number </label>
-		  	<input type="text"  placeholder='Enter Contact Number' value={contactNo} onChange={(e) => setContactNo(e.target.value)} required/>
-        <br /><br />
+            <label htmlFor="password"> Password </label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Enter Password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+            <br />
+            {/* <br /> */}
 
-        <label htmlFor="address"> Address </label>
-	  		<input type="text"  placeholder='Enter Address' value={address} onChange={(e) => setAddress(e.target.value)} required/>
-        <br /><br /> 
+            <label htmlFor="contactno"> Contact Number </label>
+            <input
+              type="text"
+              name="contactNo"
+              placeholder="Enter Contact Number"
+              value={formData.contactNo}
+              onChange={handleChange}
+              required
+            />
+            <br />
+            {/* <br /> */}
 
-        <label htmlFor="description"> Description </label>
-			  <textarea placeholder='Enter Description' value={description} onChange={(e) => setDescription(e.target.value)} />
-        <br /><br />
-        {/* <button type="button" onClick={adduser}>Submit</button> */}
-        <button type="button" onClick={adduser}>
-            {editUserId !== null ? "Update" : "Submit"}
-        </button>
-        <br /><br />
-       </div>
-    }
-    </form>
-    <table border={1}>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>FName</th>
-              <th>LName</th>
-              <th>Email</th>
-              <th>ContactNo</th>
-              <th>Address</th>
-              <th>Description</th>
-              <th>Action</th>
+            <label htmlFor="address"> Address </label>
+            <input
+              type="text"
+              name="address"
+              placeholder="Enter Address"
+              value={formData.address}
+              onChange={handleChange}
+              required
+            />
+            <br />
+            {/* <br /> */}
+
+            <label htmlFor="description"> Description </label>
+            <textarea
+              placeholder="Enter Description"
+              name="description"
+              value={formData.description}
+              onChange={handleChange} 
+            />
+            <br />
+            {/* <br /> */}
+
+            <button type="button" onClick={adduser}>
+              {editUserId !== null ? "Update" : "Submit"}
+            </button>
+            <br />
+            <br />
+          </div>
+        }
+      </form>
+      <table border={1}>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>FName</th>
+            <th>LName</th>
+            <th>Email</th>
+            <th>ContactNo</th>
+            <th>Address</th>
+            <th>Description</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.firstName}</td>
+              <td>{user.lastName}</td>
+              <td>{user.email}</td>
+              <td>{user.contactNo}</td>
+              <td>{user.address}</td>
+              <td>{user.description}</td>
+              <td>
+                <button onClick={() => deleteUser(user.id)}>Delete</button>
+              </td>
+              <td>
+                <button onClick={() => editUser(user)}>Edit</button>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {users.map((user, index) => (
-              <tr key={user.id}>
-                <td>{user.id}</td>
-                <td>{user.firstName}</td>
-                <td>{user.lastName}</td>
-                <td>{user.email}</td>
-                <td>{user.contactNo}</td>
-                <td>{user.address}</td>
-                <td>{user.description}</td>
-                <td>
-                  <button onClick={() => deleteUser(user.id)}>Delete</button>
-                </td>
-                <td>
-                  <button onClick={() => editUser(user)}>Edit</button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-  </div>
-)
-} 
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-export default App
+export default App;

dce32ee54aac804a07491d09e65b287505dedf63
MrudulSuryawanshi <suryawanshimrudul2006@gmail.com>
2026-06-15 12:30:37 +0530

    Added User component to clear App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,95 +1,11 @@
 import React from "react";
-import { useState } from "react";
 import "./App.css";
-import {Userform} from "./Components/Userform";
-import {List} from "./Components/List";
+import User from "./Sections/User";
 
 const App = () => {
-  const [users, setUsers] = useState([]);
-  const [editUserId, setEditUserId] = useState(null);
-  const [formData, setFormData] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: "",
-    contactNo: "",
-    address: "",
-    description: "",
-  });
-
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const adduser = () => {
-    if (editUserId !== null) {
-      const updatedUsers = users.map((user) => {
-        if (user.id === editUserId) {
-          return {
-            ...user,
-            ...formData,
-          };
-        }
-
-        return user;
-      });
-
-      setUsers(updatedUsers);
-      setEditUserId(null);
-    } 
-    else {
-      const user = {
-        id: Date.now(),
-        ...formData,
-      };
-      setUsers([...users, user]);
-    }
-
-    setFormData({
-      firstName: "",
-      lastName: "",
-      email: "",
-      password: "",
-      contactNo: "",
-      address: "",
-      description: "",
-    });
-  };
-
-  const deleteUser = (id) => {
-    const updatedUsers = users.filter((user) => user.id !== id);
-
-    setUsers(updatedUsers);
-  };
-
-  const editUser = (user) => {
-    setFormData({
-      firstName: user.firstName,
-      lastName: user.lastName,
-      email: user.email,
-      password: user.password || "",
-      contactNo: user.contactNo,
-      address: user.address,
-      description: user.description,
-    });
-    setEditUserId(user.id);
-  };
-
   return (
     <div>
-      <Userform
-        formData={formData}
-        handleChange={handleChange}
-        adduser={adduser}
-        editUserId={editUserId}
-      />
-      <List 
-        users={users} 
-        deleteUser={deleteUser} 
-        editUser={editUser} />
+      <User />
     </div>
   );
 };
